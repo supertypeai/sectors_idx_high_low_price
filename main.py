5c79761ce10dc42f2ec90c576849556acb23c05c
geraldbryan <geraldbryan9914@gmail.com>
2024-12-18 13:35:49 +0700

    edit name columns

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def get_data(ticker):
     stock = stock.reset_index()
     stock = stock.sort_values("Date")
     stock = stock[["Date","High","Low"]]
+    stock.columns = ["Date","High","Low"]
     stock["Date"] = pd.to_datetime(stock["Date"].dt.date)
 
     return stock
